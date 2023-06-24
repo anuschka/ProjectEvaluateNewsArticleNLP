2dd22ee798040ea0d9ae96d95b7ee6969a59d824
anuschka <sustic@gmail.com>
2023-06-24 16:08:11 +0200

    added form interactions

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -2,17 +2,18 @@ function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    Client.checkForName(formText)
-
+    let formText = document.getElementById('name').value.trim()
+    if (Client.checkForName(formText)) {
+        alert("The text field cannot be empty!")
+    } else {    
     console.log("::: Form Submitted :::")
     fetch('http://localhost:8081/test')
-    .then(res => res.json())
-    .then(function(res) {
-      
-        document.getElementById('results').innerHTML = res.confidence
-        console.log(res)
-    })
+        .then(res => res.json())
+        .then(function(res) {
+            document.getElementById('result_polarity').innerHTML = res.score_tag;
+            document.getElementById('result_subjectivity').innerHTML = res.subjectivity;
+        })
+    }
 }
 
 export { handleSubmit }
